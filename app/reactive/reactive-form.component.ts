7eed1ac40c9054be49cd691e8615fc1a31d61a5e
root <gerard.ramosm89@gmail.com>
2017-01-06 08:32:54 -0800

    reactive form created

---
+++ app/reactive/reactive-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'reactive-form',
+	templateUrl: './app/reactive/reactive-form.component.html';
+})
+
+export class ReactiveFormComponent implements OnInit {
+	ngOnInit(){
+	
+	}
+}
